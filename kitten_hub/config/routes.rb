e9c0635e6ae7128937eec936ccc3260b3d754561
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-06 16:01:46 +0000

    added devise Users

--- kitten_hub/config/routes.rb
+++ kitten_hub/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root to: "images#index"
+
   resources :images
 
 end
--- kitten_hub/spec/features/images_feature_spec.rb
+++ kitten_hub/spec/features/images_feature_spec.rb
@@ -53,7 +53,7 @@ feature 'images' do
       visit '/images'
       click_link 'Edit Kitten'
       fill_in 'Title', with: 'Miaowster'
-      click_button 'Update Title'
+      click_button 'Update'
       expect(page).to have_content 'Miaowster'
       expect(current_path).to eq '/images'
     end
