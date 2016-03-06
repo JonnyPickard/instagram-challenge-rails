bffdad00d47fa4f64730a3424f9fb3b7679a0fa3
Jonathan Pickard <Jonathandpickard@gmail.com>
2016-03-06 14:26:21 +0000

    added image model

--- kitten_hub/spec/features/images_feature_spec.rb
+++ kitten_hub/spec/features/images_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'images' do
       expect(page).to have_link "Upload an image"
     end
   end
+
+  context 'images have been added' do
+    before do
+      Image.create(title: 'Kitten')
+    end
+
+    scenario 'display images' do
+      visit '/images'
+      expect(page).to have_content("Kitten")
+      expect(page).not_to have_content("No images yet!")
+    end
+  end
 end
